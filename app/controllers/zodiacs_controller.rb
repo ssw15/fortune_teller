b7bd78026b2d791b2d149cc3ce4b58e8a80718ec
Raghu Betina <rbetina@github.com>
2015-01-14 12:18:30 -0600

    Ready for W2D2 HW

--- app/controllers/zodiacs_controller.rb
+++ app/controllers/zodiacs_controller.rb
@@ -1,4 +1,9 @@
 class ZodiacsController < ApplicationController
+  def index
+    @list_of_zodiacs = Zodiac.all
+  end
+
+
   def sign
     @zodiac = Zodiac.find_by({ :sign => params[:the_sign] })
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,27 @@ Rails.application.routes.draw do
   # PART 2: VARIABLE ROUTES
   # =======================
 
+  # Routes for adding a new Zodiac (CREATE)
+  get("/zodiacs/new", { :controller => "zodiacs", :action => "new_form" })
+  get("/create_zodiac", { :controller => "zodiacs", :action => "create" })
+
+  # Routes for reading Zodiacs (READ)
+  get("/all_zodiacs", { :controller => "zodiacs", :action => "index" })
+  get("/zodiacs/:id", { :controller => "zodiacs", :action => "show" })
+
   get("/signs/:the_sign",         { :controller => "zodiacs", :action => "sign" })
   get("/creatures/:the_creature", { :controller => "zodiacs", :action => "creature" })
+
+
+
 end
+
+
+
+
+
+
+
+
+
+
