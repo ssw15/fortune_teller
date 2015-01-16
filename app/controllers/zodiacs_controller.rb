bf216c9edc53a0f4bd26814b349fa7c0499dc579
Raghu Betina <rbetina@github.com>
2015-01-16 10:44:22 -0600

    Completed Zodiac CRUD resource

--- app/controllers/zodiacs_controller.rb
+++ app/controllers/zodiacs_controller.rb
@@ -3,8 +3,56 @@ class ZodiacsController < ApplicationController
     @list_of_zodiacs = Zodiac.all
   end
 
+  def show
+    @zodiac = Zodiac.find(params[:id])
+  end
 
-  def sign
-    @zodiac = Zodiac.find_by({ :sign => params[:the_sign] })
+  def new_form
+  end
+
+  def edit_form
+    @zodiac = Zodiac.find(params[:id])
+
+    Zodiac.find_by :id => 3
+  end
+
+  def create_row
+    @zodiac = Zodiac.new
+
+    @zodiac.sign = params[:sign]
+    @zodiac.creature = params[:creature]
+    @zodiac.fortune = params[:fortune]
+
+    @zodiac.save
+
+    redirect_to "http://localhost:3000/zodiacs/#{@zodiac.id}", :notice => "Successfully created zodiac."
+  end
+
+  def update_row
+    @zodiac = Zodiac.find(params[:id])
+
+    @zodiac.sign = params[:sign]
+    @zodiac.creature = params[:creature]
+    @zodiac.fortune = params[:fortune]
+
+    @zodiac.save
+
+    redirect_to("http://localhost:3000/zodiacs/#{@zodiac.id}", :notice => "Successfully updated zodiac.")
+  end
+
+  def destroy_row
+    @zodiac = Zodiac.find(params[:id])
+
+    @zodiac.destroy
+
+    redirect_to("http://localhost:3000/all_zodiacs", :notice => "Successfully destroyed zodiac.")
   end
 end
+
+
+
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,21 +4,23 @@ Rails.application.routes.draw do
   get("/lucky",   { :controller => "fortunes", :action => "lucky_numbers" })
   get("/unlucky", { :controller => "fortunes", :action => "unlucky_numbers" })
 
-  # PART 2: VARIABLE ROUTES
-  # =======================
+  # ROUTES FOR ZODIAC RESOURCE
+  # ==========================
 
   # Routes for adding a new Zodiac (CREATE)
-  get("/zodiacs/new", { :controller => "zodiacs", :action => "new_form" })
-  get("/create_zodiac", { :controller => "zodiacs", :action => "create" })
+  get "/zodiacs/new", :controller => "zodiacs", :action => "new_form"
+  get("/create_zodiac", { :controller => "zodiacs", :action => "create_row" })
 
   # Routes for reading Zodiacs (READ)
   get("/all_zodiacs", { :controller => "zodiacs", :action => "index" })
   get("/zodiacs/:id", { :controller => "zodiacs", :action => "show" })
 
-  get("/signs/:the_sign",         { :controller => "zodiacs", :action => "sign" })
-  get("/creatures/:the_creature", { :controller => "zodiacs", :action => "creature" })
+  # Routes for updating Zodiacs (UPDATE)
+  get("/zodiacs/:id/edit", { :controller => "zodiacs", :action => "edit_form" } )
+  get("/update_zodiac/:id", { :controller => "zodiacs", :action => "update_row" })
 
-
+  # Routes for deleting Zodiacs (DELETE)
+  get("/delete_zodiac/:id", { :controller => "zodiacs", :action => "destroy_row" })
 
 end
 
